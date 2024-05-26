eff35a1eab7a06f4c7951c15b300f1fee9d2a3fc
oussama <oussamaelatmani3@gmail.com>
2024-05-26 16:50:41 +0100

    change seeder

--- back-end/database/seeders/DesignerSeeder.php
+++ back-end/database/seeders/DesignerSeeder.php
@@ -14,11 +14,46 @@ class DesignerSeeder extends Seeder
      */
     public function run(): void
     {
-        Designer::create([
-            'first_name' => 'Oussama',
-            'last_name' => 'Othmani',
-            'email' => 'designer@ofppt.com',
-            'password' => Hash::make('ofppt'),
-        ]);
+        $data = [
+            // Mle Affecté Présentiel Actif    Formateur Affecté Présentiel Actif
+            ['first_name' => 'Fatima', 'last_name' => 'Moustati', 'email' => 'fatima.moustati@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Kaoutar', 'last_name' => 'Meddri', 'email' => 'kaoutar.meddri@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Hassan', 'last_name' => 'Khoullak', 'email' => 'hassan.khoullak@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Faical', 'last_name' => 'Essaiydy', 'email' => 'faical.essaiydy@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Khadija', 'last_name' => 'Traf', 'email' => 'khadija.traf@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Mostafa', 'last_name' => 'Zahouani', 'email' => 'mostafa.zahouani@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Mohammed', 'last_name' => 'Salahi', 'email' => 'mohammed.salahi@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Abdeltif', 'last_name' => 'Lahrari', 'email' => 'abdeltif.lahrari@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Said', 'last_name' => 'Fallah', 'email' => 'said.fallah@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Youssef', 'last_name' => 'Fatah', 'email' => 'youssef.fatah@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Saida', 'last_name' => 'Kraiouch', 'email' => 'saida.kraiouch@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Karim', 'last_name' => 'Hassoune', 'email' => 'karim.hassoune@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Abdelhadi', 'last_name' => 'Souda', 'email' => 'abdelhadi.souda@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Ahmed', 'last_name' => 'Elharti', 'email' => 'ahmed.elharti@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Boubker', 'last_name' => 'Mouti', 'email' => 'boubker.mouti@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Ilham', 'last_name' => 'Barhdadi', 'email' => 'ilham.barhdadi@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Ayoub', 'last_name' => 'Rajil', 'email' => 'ayoub.rajil@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Khadija', 'last_name' => 'Chekrani', 'email' => 'khadija.chekrani@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Zineb', 'last_name' => 'Chaki', 'email' => 'zineb.chaki@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Anass', 'last_name' => 'Khalfi', 'email' => 'anass.khalfi@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Abdelkrim', 'last_name' => 'Belasri', 'email' => 'abdelkrim.belasri@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Redouane', 'last_name' => 'Souhail', 'email' => 'redouane.souhail@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Said', 'last_name' => 'Nait El Haj', 'email' => 'said.naitelhaj@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Adil', 'last_name' => 'Anibat', 'email' => 'adil.anibat@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Badr-eddine', 'last_name' => 'Aaqil', 'email' => 'badr-eddine.aaqil@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Mohamed', 'last_name' => 'Ait Tahar', 'email' => 'mohamed.aittahar@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Mohamed', 'last_name' => 'Elmechbouk', 'email' => 'mohamed.elmechbouk@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Ahmed', 'last_name' => 'Eddanguir', 'email' => 'ahmed.eddanguir@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Abdelmajid', 'last_name' => 'Maziane', 'email' => 'abdelmajid.maziane@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Khalid', 'last_name' => 'Naji', 'email' => 'khalid.naji@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Mostafa', 'last_name' => 'Lagmiri', 'email' => 'mostafa.lagmiri@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Abdelghani', 'last_name' => 'Ennaciri', 'email' => 'abdelghani.ennaciri@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Salaheddine', 'last_name' => 'Bahri', 'email' => 'salaheddine.bahri@ofppt.com', 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Moussa', 'last_name' => 'Essaadaoui', 'email' => 'moussa.essaadaoui@ofppt.com', 'password' => Hash::make('ofppt')],
+        ];
+
+        foreach ($data as $designerData) {
+            Designer::create($designerData);
+        }
     }
 }
--- back-end/database/seeders/ValidatorSeeder.php
+++ back-end/database/seeders/ValidatorSeeder.php
@@ -15,12 +15,28 @@ class ValidatorSeeder extends Seeder
      */
     public function run(): void
     {
-        Validator::create([
-            'first_name' => 'Oussama',
-            'last_name' => 'Othmani',
-            'email' => 'validator@ofppt.com',
-            'filiere_id' => Filiere::all()->random()->id,
-            'password' => Hash::make('ofppt'),
-        ]);
+        $data = [
+            // Communication & Soft Skills
+            ['first_name' => 'Kaoutar', 'last_name' => 'Meddri', 'email' => 'kaoutar@ofppt.com', 'filiere_id' => Filiere::where('name', 'Communication & Soft Skills')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Khadija', 'last_name' => 'Traf', 'email' => 'khadija@ofppt.com', 'filiere_id' => Filiere::where('name', 'Communication & Soft Skills')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Khadija', 'last_name' => 'Chekrani', 'email' => 'khadija.chekrani@ofppt.com', 'filiere_id' => Filiere::where('name', 'Communication & Soft Skills')->first()->id, 'password' => Hash::make('ofppt')],
+            // Génie électrique
+            ['first_name' => 'Faical', 'last_name' => 'Essaiydy', 'email' => 'faical@ofppt.com', 'filiere_id' => Filiere::where('name', 'Génie électrique')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Mohammed', 'last_name' => 'Salahi', 'email' => 'mohammed@ofppt.com', 'filiere_id' => Filiere::where('name', 'Génie électrique')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Said', 'last_name' => 'Fallah', 'email' => 'said@ofppt.com', 'filiere_id' => Filiere::where('name', 'Génie électrique')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Abdelhadi', 'last_name' => 'Souda', 'email' => 'abdelhadi@ofppt.com', 'filiere_id' => Filiere::where('name', 'Génie électrique')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Ilham', 'last_name' => 'Barhdadi', 'email' => 'ilham@ofppt.com', 'filiere_id' => Filiere::where('name', 'Génie électrique')->first()->id, 'password' => Hash::make('ofppt')],
+            // Génie Mécanique
+            ['first_name' => 'Badr-eddine', 'last_name' => 'Aaqil', 'email' => 'badr-eddine@ofppt.com', 'filiere_id' => Filiere::where('name', 'Génie Mécanique')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Ahmed', 'last_name' => 'Eddanguir', 'email' => 'ahmed@ofppt.com', 'filiere_id' => Filiere::where('name', 'Génie Mécanique')->first()->id, 'password' => Hash::make('ofppt')],
+            // Métiers de l’Automobile
+            ['first_name' => 'Ayoub', 'last_name' => 'Rajil', 'email' => 'ayoub@ofppt.com', 'filiere_id' => Filiere::where('name', 'Métiers de l’Automobile')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Khalid', 'last_name' => 'Naji', 'email' => 'khalid@ofppt.com', 'filiere_id' => Filiere::where('name', 'Métiers de l’Automobile')->first()->id, 'password' => Hash::make('ofppt')],
+            ['first_name' => 'Salaheddine', 'last_name' => 'Bahri', 'email' => 'salaheddine@ofppt.com', 'filiere_id' => Filiere::where('name', 'Métiers de l’Automobile')->first()->id, 'password' => Hash::make('ofppt')],
+        ];
+
+        foreach ($data as $validatorData) {
+            Validator::create($validatorData);
+        }
     }
 }
